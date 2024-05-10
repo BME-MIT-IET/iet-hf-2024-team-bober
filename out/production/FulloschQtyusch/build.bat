5a34c91b263fe738d100e51e9fb296e8de2eaad7
szebo <szebo12@gmail.com>
2024-05-10 16:28:19 +0200

    out mappa törolve

--- out/production/FulloschQtyusch/build.bat
+++
@@ -1,4 +0,0 @@
-REM A dokumentacio alapjan elvegzi a forditast.
-javac -encoding UTF8 virologist.Main.java control/*.java model/*java model/agents/*java model/codes/*java model/equipments/*java model/map/*.java model/strategy/*.java view/*.java
-
-jar cvfm0 ..\Qtyusch.jar META-INF/MANIFEST.MF *
