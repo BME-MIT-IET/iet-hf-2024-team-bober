f355c089c2aaca098b11d000ccc06b5fc75e55de
davidstvn <id.remenyi@gmail.com>
2024-05-10 13:42:01 +0200

    Easy build and run update.
    Build and run batch file-okat keszitettem, ahol be lehet allitani a forditashoz hasznalt java verziojat, valamint a futtatasnal a map-ot.

---
+++ FulloschQtyusch/src/build.bat
@@ -0,0 +1,4 @@
+REM A dokumentacio alapjan elvegzi a forditast.
+javac -encoding UTF8 Main.java control/*.java model/*java model/agents/*java model/codes/*java model/equipments/*java model/map/*.java model/strategy/*.java view/*.java
+
+jar cvfm0 ..\Qtyusch.jar META-INF/MANIFEST.MF *
