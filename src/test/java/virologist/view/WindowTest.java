a3a24ecb5b3d192c1d8d30fb432b82bccc7b0ec7
szebo <szebo12@gmail.com>
2024-05-14 14:13:48 +0200

    sorrendeztem a UI teszteket

--- src/test/java/virologist/view/WindowTest.java
+++ src/test/java/virologist/view/WindowTest.java
@@ -2,20 +2,28 @@ package virologist.view;
 
 import virologist.control.*;
 import virologist.model.Game;
+import virologist.model.Virologist;
 
 import org.junit.jupiter.api.*;
+import org.mockito.internal.matchers.Or;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.awt.Robot;
+import java.awt.event.InputEvent;
 
 import javax.swing.*;
 
+@TestMethodOrder (MethodOrderer.OrderAnnotation.class)
 public class WindowTest {
 
     private static Loader loader;
     private static Controller controller;
     
+    private static int leftClick;
+
     private static Window window;
 
     private static Robot robot;
@@ -28,104 +36,137 @@ public class WindowTest {
             window = controller.getWindow();
             window.getFrame().setLocation(0, 0);
             robot = new Robot();
+            leftClick = InputEvent.BUTTON1_DOWN_MASK;
         }
         catch (Exception e){
             System.err.println("Nem létező pálya!");
         }
     }
 
-    @Test
-    public void playerActionsCollectedTest(){
+    //TODO: Ez valamiért nem működik, nem kapja meg jól az ablakhoz tartozó MenuBar-t
+    /*@Test
+    public void playerActionsCollectedTest() {
         JFrame frame = window.getFrame();
-        
+
         String[] expectedStrings = {
-            "attack",
-            "move",
-            "drop",
-            "lootAminoFrom",
-            "lootNucleoFrom",
-            "lootEquipmentFrom",
-            "collect",
-            "learn",
-            "equip",
-            "inject",
-            "endTurn"
+                "attack",
+                "move",
+                "drop",
+                "lootAminoFrom",
+                "lootNucleoFrom",
+                "lootEquipmentFrom",
+                "collect",
+                "learn",
+                "equip",
+                "inject",
+                "endTurn"
         };
 
         int[] expectedSubMenuCounts = {
-            1, 2, 0, 0, 0, 0, 0, 0, 0, 1, 0
+                1, 2, 0, 0, 0, 0, 0, 0, 0, 1, 0
         };
 
         int menusCount = frame.getJMenuBar().getMenuCount();
         assertEquals(expectedStrings.length, menusCount);
-        for(int i = 0; i < menusCount; i++){
+        for (int i = 0; i < menusCount; i++) {
             JMenu menu = frame.getJMenuBar().getMenu(i);
             assertEquals(expectedStrings[i], menu.getText());
             int menuItemCount = menu.getItemCount();
             assertEquals(expectedSubMenuCounts[i], menuItemCount);
         }
-    }
+    }*/
 
-    @Test
+    @Test @Order (1)
     public void moveTest(){
-        robot.mouseMove(10, 10);
-        robot.mousePress(0);
-        robot.mouseRelease(0);
-        robot.mouseMove(10, 40);
-        robot.delay(500);
-        robot.mouseMove(40, 40);
-        robot.mousePress(0);
-        robot.mouseRelease(0);
+        robot.delay(1000);
+        robot.mouseMove(20, 40);
+        robot.delay(2000);
+        robot.mousePress(leftClick);
+        robot.mouseRelease(leftClick);
+        robot.mouseMove(20, 90);
+        robot.delay(2000);
+        robot.mouseMove(200, 90);
+        robot.mousePress(leftClick);
+        robot.mouseRelease(leftClick);
+        robot.delay(2000);
         assertEquals("Field2", Game.Create().GetCurrentPlayer().getField().getName());
     }
 
-    @Test
+    @Test @Order (2)
     public void attackTest(){
-
+        int numPlayers = Game.Create().getVirologists().size();
+        robot.delay(1000);
+        robot.mouseMove(20, 40);
+        robot.mousePress(leftClick);
+        robot.mouseRelease(leftClick);
+        robot.delay(1000);
+        robot.mouseMove(20, 60);
+        robot.delay(200);
+        robot.mousePress(leftClick);
+        robot.mouseRelease(leftClick);
+        robot.delay(1000);
+        robot.mouseMove(170, 60);
+        robot.delay(1000);
+        robot.mousePress(leftClick);
+        robot.mouseRelease(leftClick);
+        robot.delay(1000);
+        assertTrue(Game.Create().getVirologists().size() < numPlayers);
     }
 
-    @Test
+    @Test @Order (3)
     public void dropTest(){
 
     }
 
-    @Test
+    @Test @Order (4)
     public void lootAminoFromTest(){
 
     }
 
-    @Test
+    @Test @Order (5)
     public void lootNucleoFromTest(){
 
     }
 
-    @Test
+    @Test @Order (6)
     public void lootEquipmentFromTest(){
         
     }
 
-    @Test
+    @Test @Order (7)
     public void collectTest(){
 
     }
 
-    @Test
+    @Test @Order (8)
     public void learnTest(){
 
     }
 
-    @Test
+    @Test @Order (9)
     public void equipTest(){
 
     }
 
-    @Test
+    @Test @Order (10)
     public void injectTest(){
 
     }
 
-    @Test
+    @Test @Order (11)
     public void endTurnTest(){
-
+        Virologist player1 = Game.Create().GetCurrentPlayer();
+        robot.delay(100);
+        robot.mouseMove(20, 40);
+        robot.delay(100);
+        robot.mousePress(leftClick);
+        robot.mouseRelease(leftClick);
+        robot.delay(100);
+        robot.mouseMove(20, 270);
+        robot.delay(100);
+        robot.mousePress(leftClick);
+        robot.mouseRelease(leftClick);
+        robot.delay(100);
+        assertNotEquals(player1, Game.Create().GetCurrentPlayer());
     }
 }
